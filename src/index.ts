5f0220b754c1b0d7b0dee7d862125c08e121d36b
Lucas Gabriel Moya <lucasgabrielfilm@gmail.com>
2021-12-07 11:57:20 -0300

    creating a server with express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello World");
+import express, { Request, Response, NextFunction } from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) =>{
+    res.status(200).send({foo:'bar'});
+});
+
+
+app.listen(port, () => {
+    console.log(`'Aplicacao rodando na porta ${port}!`);
+});
