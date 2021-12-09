773ff4f9b8459d0c3e7ec88166444cb9941aae07
Lucas Gabriel Moya <lucasgabrielfilm@gmail.com>
2021-12-09 10:14:20 -0300

    get, post, put, delete methods already created

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
-import express, { Request, Response, NextFunction } from 'express';
-import userRouter from './routes/users.route';
+import express from 'express';
+import statusRoute from './routes/status.route';
+import usersRoute from './routes/users.route';
 
 const app = express();
 const port = 3000;
 
-app.use(userRouter);
+//Configurações da aplicação
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/status', (req: Request, res: Response, next: NextFunction) =>{
-    res.status(200).send({foo:'Success!'});
-});
+//Configurações de Rotas
+app.use(statusRoute);
+app.use(usersRoute);
 
-
+//Inicialização do servidor
 app.listen(port, () => {
     console.log(`'Aplicacao rodando no porta ${port}!`);
 });
